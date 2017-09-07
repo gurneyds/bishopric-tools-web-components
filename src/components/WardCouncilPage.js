4d0eca3a9453bc2eea76bde1ecf09faaf6ccff77
David Gurney <gurneyds@familysearch.org>
2017-09-07 07:33:09 -0600

    Remote obsolete components

--- src/components/WardCouncilPage.js
+++ src/components/WardCouncilPage.js
@@ -3,37 +3,37 @@ require('./ListPicker');
 require('./DetailsView');
 
 (function() {
-  let template = `
-    <style>
-      .container {
-  	     border: 1px solid black;
-         margin: 5px;
-         padding: 5px;
-       }
-    </style>
-    <div class='container'>
-      <h2>This is the Ward Council page</h2>
-      <list-picker></list-picker>
-      <details-view></details-view>
-    </div>
-  `;
+	let template = `
+	<style>
+		.container {
+			border: 1px solid black;
+			margin: 5px;
+			padding: 5px;
+		}
+	</style>
+	<div class='container'>
+		<h2>This is the Ward Council page</h2>
+		<list-picker></list-picker>
+		<details-view></details-view>
+	</div>
+	`;
 
-  class WardCouncilPage extends HTMLElement {
-    constructor() {
-      super();
-      consol.log("WardCouncilPage constructor called");
-      // Shadow Root
-      this._root = this.attachShadow({mode: "open"});
-    }
+	class WardCouncilPage extends HTMLElement {
+		constructor() {
+			super();
+			consol.log("WardCouncilPage constructor called");
+			// Shadow Root
+			this._root = this.attachShadow({mode: "open"});
+		}
 
-    createdCallback() {
-      this.createShadowRoot().innerHTML = template;
-    }
+		createdCallback() {
+			this.createShadowRoot().innerHTML = template;
+		}
 
-    setData(data) {
-      console.log('wardCouncilPage setData called');
-    }
-  }
+		setData(data) {
+			console.log('wardCouncilPage setData called');
+		}
+	}
 
-  document.registerElement('ward-council-page', WardCouncilPage);
+	document.registerElement('ward-council-page', WardCouncilPage);
 })();
