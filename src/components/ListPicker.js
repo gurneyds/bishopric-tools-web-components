60ae431722e16d41ba63af113fab13ab5f4d3e1b
David Gurney <gurneyds@yahoo.com>
2017-09-13 15:26:24 -0600

    Add a stylesheet

--- src/components/ListPicker.js
+++ src/components/ListPicker.js
@@ -8,28 +8,71 @@
 			margin: 5px;
 			padding: 5px;
 		}
+		.list-container {
+			border: 1px solid #ddd;
+		}
+		.list-container > div {
+		}
+		.list-container > div:hover {
+			background: yellow;
+		}
+		.delete-button {
+			background: url(images/delete.png) no-repeat 6px center;
+		}
+		.close {
+			position: absolute;
+			top: 15px;
+			right: 35px;
+			color: #f1f1f1;
+			font-size: 40px;
+			font-weight: bold;
+			transition: 0.3s;
+		}
+
+		.close:hover,
+		.close:focus {
+			color: #bbb;
+			text-decoration: none;
+			cursor: pointer;
+		}
 	</style>
 	<div class='container'>
 		<h2>This is the list picker page</h2>
+		<div class='list-container'>
+			<div>Item one <span class="close">x</span></div>
+			<div>Item two</div>
+			<div>Item three</div>
+			<div>Item four</div>
+		</div>
 	</div>
 	`;
 
+	/*
+	The purpose of this component is to render a list of items and do the following:
+	1-) Emit an event when an item is selected
+	2-) Emit an event when a delete icon is selected
+	3-) Emit an event when the user wants to create a new item
+	4-) Listen for data change events and re-render the list
+	*/
 	class ListPicker extends HTMLElement {
 		constructor() {
 			super();
-			consol.log("ListPicker constructor called");
-			// Shadow Root
-			this._root = this.attachShadow({mode: "open"});
+			this.attachShadow({mode: "open"}).innerHTML = template;
 		}
 
-		createdCallback() {
-			this.createShadowRoot().innerHTML = template;
+		connectedCallback() {
+			console.log('connectedCallback called');
 		}
 
 		setData(data) {
 			console.log('ListPicker setData called');
+			this.data = data;
+		}
+
+		_render() {
+
 		}
 	}
 
-	document.registerElement('list-picker', ListPicker);
+	customElements.define('list-picker', ListPicker);
 })();
--- webpack.config.js
+++ webpack.config.js
@@ -17,16 +17,16 @@ module.exports = {
 			}
 		],
 		rules: [
-    {
-      test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['env']
-        }
-      }
-    },
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+					presets: ['env']
+					}
+				}
+			},
 		{
 			test: /\.css$/,
 			exclude: /node_modules/,
